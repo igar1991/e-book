5b9a580cb24fe979d88142ad2873fd34fe37e951
iiggaarr <iiggaarr19912004@gmail.com>
2021-06-08 13:42:22 +0300

    nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logo from "../file/logo.png";
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light mb-4">
-      <form className="container-fluid justify-content-end">
-          <NavLink to="/">
-        <button className="btn btn btn-outline-primary m-2 btn-lg" type="button">
-         Электронные тетради
-        </button>
+      <div className="container-fluid d-flex flex-row">
+      <NavLink to="/">
+        <img src={logo} alt="" width="80%" height="80%" />
+      </NavLink>
+      <form>
+        <NavLink to="/">
+          <button
+            className="btn btn btn-outline-primary m-2 btn-lg"
+            type="button"
+          >
+            Электронные тетради
+          </button>
         </NavLink>
         <NavLink to="/result">
-        <button className="btn btn-outline-success" type="button">
-          Результаты
-        </button>
+          <button className="btn btn-outline-success" type="button">
+            Результаты
+          </button>
         </NavLink>
       </form>
+      </div>
     </nav>
   );
 };
